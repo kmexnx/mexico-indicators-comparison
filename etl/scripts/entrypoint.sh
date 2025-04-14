ffe176ff53ac1a331e0b617ff74aa3a1d9bbf4a2
kmexnx <46535537+kmexnx@users.noreply.github.com>
2025-04-13 18:55:45 -0600

    Añadir script de entrypoint para Airflow

--- etl/scripts/entrypoint.sh
+++ etl/scripts/entrypoint.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Initialize the Airflow database
-airflow db init
+# Initialize the Airflow database if it doesn't exist
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+    airflow db init
+fi
 
 # Create an Airflow user if it doesn't exist
-airflow users create \
-    --username admin \
-    --password admin \
-    --firstname Admin \
-    --lastname User \
-    --role Admin \
-    --email admin@example.com
+airflow users list | grep -q "admin" || \
+    airflow users create \
+        --username admin \
+        --password admin \
+        --firstname Admin \
+        --lastname User \
+        --role Admin \
+        --email admin@example.com
 
 # Start Airflow based on the command passed
 exec airflow "$@"
